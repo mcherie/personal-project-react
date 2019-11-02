c4a6e7bf0c55676555d24cf27336a0701f246148
Cher <red_phoenix417@yahoo.com>
2019-11-02 10:57:35 -0400

    created state to be able to render result component

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
+import React from "react";
 
-export const Result = (username) => {
+export const Result = ({ result }) => {
+  const { username, parentRepoNames, pullRequests } = result;
+  return (
+    <div>
+      <h2 className="username-header"> {username} </h2>
 
-return (
+      <h4> Recent Forks </h4>
+      {parentRepoNames.map(each => {
+        console.log(each);
+        
+      })}
+      
+      <h6>Forked from: </h6>
 
-  <div>
-
-    <h2 className="username-header"> {username} </h2>
-
-    <h4> Recent Forks </h4>
-
-
-    <h4> Recent Pull Requests </h4>
-
-  </div>
-
-)
-
-}
+      <h4> Recent Pull Requests </h4>
+      <h6>Status: </h6>
+    </div>
+  );
+};
 
 export default Result;
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,55 +1,76 @@
-import React, { useState } from "react"
-import Result from "./result"
+import React, { useState } from "react";
+import Result from "./result";
 
 export const Search = () => {
+  const [username, setUsername] = useState("");
+  const [results, setResults] = useState({});
 
-  const [ username, setUsername ] = useState("")
 
-  const handleGetUser = async (username) => {
-    try {
-      const repos_URL = `https://api.github.com/users/${username}/repos`
-      const events_URL = `https://api.github.com/users/${username}/events`
-
-      // Below is to get the forked repos
-      const reposResponse = await fetch(repos_URL)
-      const receivedRepos = await reposResponse.json()
-      const forkedProjects = receivedRepos.filter(item => item.fork === true )
-      console.log("forkeds are:", forkedProjects)
-
-      // Below is to get Pull Requests
-      const eventsResponse = await fetch(events_URL)
-      const receivedEvents = await eventsResponse.json()
-      const pullRequests = receivedEvents.filter(item => item.type === "PullRequestEvent")
-      console.log("PRs are:", pullRequests)
-
-      const results = [forkedProjects, pullRequests]
-
-      return results
-
-    } catch (err) {
-      console.log(err.message)
-    }
+  const forkedRepos = async (full_name) => {
+    const forkedRepos_URL = `https://api.github.com/repos/${full_name}`;
+    const getForkedRepos = await fetch(forkedRepos_URL);
+    const forkedReposJson = await getForkedRepos.json()
+    console.log("parent full name is", forkedReposJson.parent.full_name);
+    return forkedReposJson.parent.full_name;
   }
 
-  return (
-    <div> 
-      <p>Github Username:</p>
-    <input value={username} onChange={e => setUsername(e.target.value)}/> 
-    <div>
-        <button type="button" onClick={() => {
-          handleGetUser(username)
-          return (
-            <Result 
-              username={username} 
-              // forkedProjects={forkedProjects} 
-              // pullRequests={pullRequests} 
-            />
-          )
-        }}>GET USER</button>
-      </div>
-    </div>
+  const handleGetUser = async username => {
+    try {
+      const repos_URL = `https://api.github.com/users/${username}/repos`;
+      const events_URL = `https://api.github.com/users/${username}/events`;
 
-  )
-}
+      // Below is to get the forked repos
+      const reposResponse = await fetch(repos_URL);
+      const receivedRepos = await reposResponse.json();
+      const forkedProjects = receivedRepos.filter(item => item.fork === true);
+      // console.log("forkeds are:", forkedProjects);
+      const parentRepoNames = forkedProjects.map(each => {
+        return forkedRepos(each.full_name);
+      });
+
+      // Below is to get Pull Requests
+      const eventsResponse = await fetch(events_URL);
+      const receivedEvents = await eventsResponse.json();
+      const pullRequests = receivedEvents.filter(
+        item => item.type === "PullRequestEvent"
+      );
+      // console.log("PRs are:", pullRequests);
+
+      const results = { username, parentRepoNames, pullRequests };
+
+      setResults(results);
+
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const searchUser = () => {
+    return (
+      <div>
+        <p>Github Username:</p>
+        <input value={username} onChange={e => setUsername(e.target.value)} />
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              handleGetUser(username);
+            }}
+          >
+            GET USER
+          </button>
+        </div>
+      </div>
+    );
+  };
+
+  const displayResult = () => {
+    return <Result result={results} />;
+  };
+
+  return (
+    <div>{Object.keys(results).length ? displayResult() : searchUser()}</div>
+  );
+};
 
 export default Search;
